5b1b13af5ef18690a80ba39b8515235cbf3df261
Han <huhan8807@gmail.com>
2019-04-14 16:37:25 +0800

    update build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,8 @@
 mkdir build
 cd build
-cmake -GNinja -DJP2KFLAG=OFF -Dpybindings=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ../isis
+cmake -GNinja ../isis -DJP2KFLAG=OFF \
+      -Dpybindings=OFF \
+      -DbuildTests=OFF \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX 
 ninja install
