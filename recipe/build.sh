480b34d4701352a7931913ed1d7f59b628fc7d79
Han <huhan8807@gmail.com>
2019-04-14 17:13:37 +0800

    ignore warning

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,11 @@
 mkdir build
 cd build
-cmake -GNinja ../isis -DJP2KFLAG=OFF \
+cmake -GNinja ../isis \
+      -DJP2KFLAG=OFF \
       -Dpybindings=OFF \
       -DbuildTests=OFF \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX 
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations -Wno-misleading-indentation"
+
 ninja install
